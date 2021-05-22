a7d82440ea378a2ed6f6f4eec4c3fb7147c42b18
Rishab Dudhia <rishabdudhia@gmail.com>
2021-05-22 10:14:17 -0700

    Finished part 2

--- test/tests.py
+++ test/tests.py
@@ -15,20 +15,24 @@
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
 tests = [ {'description': 'This test will run first.',
-    'steps': [ {'inputs': [('PINA',<val>)], 'iterations': 1 } ],
-    'expected': [('PORT',<val>)],
+    'steps': [ {'inputs': [('PINB',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x00)],
     },
-    {'description': 'This test will run second.',
-    'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
-        {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
-        {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
-        {'inputs': [('PIN',<val>)], 'time': 600}, ],
-    'expected': [('PORT',<val>)],
+    {'description': 'This test will run first.',
+    'steps': [ {'inputs': [('PINB',0x80)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x02)],
     },
+    #{'description': 'This test will run second.',
+    #'steps': [ {'inputs': [('PIN', <val>)],'iterations': 1}, # Set PIN to val then run one iteration
+    #    {'inputs': [('PIN',<val>)], 'time': 300 }, # Set PIN to val then run 300 ms
+    #    {'inputs': [('PIN',<val>)], 'iterations': 1, 'expected': [('PORT',<val>)]}, 
+    #    {'inputs': [('PIN',<val>)], 'time': 600}, ],
+    #'expected': [('PORT',<val>)],
+    #},
     ]
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['<function>::<static-var>','PORTB']
+watch = ['lstate', 'outB']
 
