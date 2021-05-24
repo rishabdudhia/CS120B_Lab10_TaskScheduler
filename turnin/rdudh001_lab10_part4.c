8997f7cc0c2b85501fd02b003e75804cc31f6053
rishabdudhia <81605069+rishabdudhia@users.noreply.github.com>
2021-05-23 20:24:40 -0700

    Update rdudh001_lab10_part4.c

--- turnin/rdudh001_lab10_part4.c
+++ turnin/rdudh001_lab10_part4.c
@@ -8,6 +8,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
+ *	Youtube link: https://www.youtube.com/watch?v=wGW0XkKZIpc
  */
 
 #include <avr/io.h>
